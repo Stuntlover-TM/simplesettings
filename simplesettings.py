2b61e02db45e421a4c617aafee48bbe990391e0f
Stuntlover <101748104+Stuntlover-TM@users.noreply.github.com>
2023-02-21 17:10:08 +0100

    Create simplesettings.py

---
+++ simplesettings.py
@@ -0,0 +1,34 @@
+import csv
+import ast
+import os
+
+def _init():
+    if not os.path.isfile(".settings"):
+        with open(".settings", "w", newline="") as cachefile:
+            writer = csv.writer(cachefile, delimiter="|")
+            writer.writerow(["name", "value"])
+
+def clear():
+    with open(".settings", "w", newline="") as cachefile:
+        writer = csv.writer(cachefile, delimiter="|")
+        writer.writerow(["name", "value"])  
+
+def save(name, value):
+    _init()
+    with open(".settings", "a", newline="") as cachefile:
+        writer = csv.writer(cachefile, delimiter="|")
+        writer.writerow([name, value])
+
+def load():
+    _init()
+    with open(".settings", "r") as cachefile:
+        reader = csv.DictReader(cachefile, delimiter="|")
+        cachedicts = list(reader)
+        return_cache = {}
+
+        for dictionary in cachedicts:
+            for i, j in dictionary.items():
+                try:
+                    return_cache[dictionary["name"]] = ast.literal_eval(str(dictionary["value"]))# if isinstance(j, dict) else None
+                except: continue
+        return return_cache
