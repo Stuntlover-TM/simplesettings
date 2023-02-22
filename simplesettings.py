bd838db0f1299f937f85edf092cb125ad202f873
Stuntlover <101748104+Stuntlover-TM@users.noreply.github.com>
2023-02-22 13:06:42 +0100

    Add load string function

--- simplesettings.py
+++ simplesettings.py
@@ -1,6 +1,8 @@
 import csv
 import ast
+import io
 import os
+
 
 def _init():
     if not os.path.isfile(".settings"):
@@ -8,16 +10,19 @@ def _init():
             writer = csv.writer(cachefile, delimiter="|")
             writer.writerow(["name", "value"])
 
+
 def clear():
     with open(".settings", "w", newline="") as cachefile:
         writer = csv.writer(cachefile, delimiter="|")
         writer.writerow(["name", "value"])  
+
 
 def save(name, value):
     _init()
     with open(".settings", "a", newline="") as cachefile:
         writer = csv.writer(cachefile, delimiter="|")
         writer.writerow([name, value])
+
 
 def load():
     _init()
@@ -32,3 +37,17 @@ def load():
                     return_cache[dictionary["name"]] = ast.literal_eval(str(dictionary["value"]))# if isinstance(j, dict) else None
                 except: return_cache[dictionary["name"]] = j
         return return_cache
+
+
+def loads(str):
+    _init()
+    reader = csv.DictReader(io.StringIO(str.strip()), delimiter="|")
+    cachedicts = list(reader)
+    return_cache = {}
+
+    for dictionary in cachedicts:
+        for i, j in dictionary.items():
+            try:
+                return_cache[dictionary["name"]] = ast.literal_eval(str(dictionary["value"]))# if isinstance(j, dict) else None
+            except: return_cache[dictionary["name"]] = j
+    return return_cache
